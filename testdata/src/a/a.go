a0ccf2d2c4eeb8c2263113387b5d47609eb7ff8a
yusuke.kadowaki <yusuke.kadowaki.1231@gmail.com>
2023-02-23 22:03:39 +0900

    Add failing test

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,11 @@
 package a
 
+import "github.com/rs/zerolog/log"
+
 func f() {
 	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
+	var gopher int // want "identifier is gopher"
 	print(gopher)  // want "identifier is gopher"
+	var err error
+	log.Error().Err(err) // want "missing Msg() or Send()"
 }
